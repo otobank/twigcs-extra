c332b04084262ccef9cd6026211980468166ead4
sasezaki <sasezaki@gmail.com>
2021-02-25 19:50:45 +0900

    init

---
+++ src/Rule/CaseSensitiveInclude.php
@@ -0,0 +1,109 @@
+<?php declare(strict_types=1);
+
+namespace Otobank\TwigcsExtra\Rule;
+
+use FriendsOfTwig\Twigcs\Rule\AbstractRule;
+use FriendsOfTwig\Twigcs\Rule\RuleInterface;
+use FriendsOfTwig\Twigcs\TwigPort\Token;
+use FriendsOfTwig\Twigcs\TwigPort\TokenStream;
+
+final class CaseSensitiveInclude extends AbstractRule implements RuleInterface
+{
+    /**
+     * @var array<string>
+     */
+    private $paths;
+
+    /**
+     * @var array
+     */
+    private $globPaths;
+
+    public function __construct(int $severity, array $paths)
+    {
+        parent::__construct($severity);
+
+        // todo validate paths are absolute dirs
+        $this->paths = $paths;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function check(TokenStream $tokens)
+    {
+        $violations = [];
+
+        do {
+            $token = $tokens->getCurrent();
+            if (!(Token::NAME_TYPE === $token->getType() &&
+                'include' === $token->getValue())) {
+                continue;
+            }
+
+            $include = $tokens->look(2)->getValue();
+
+            if (!$this->exists($include)) {
+                $violations[] = $this->createViolation(
+                    $tokens->getSourceContext()->getPath(),
+                    $token->getLine(),
+                    $token->getColumn(),
+                    sprintf('include "%s" is not valid path.', $include)
+                );
+            }
+
+        } while (!$tokens->isEOF() && $tokens->next());
+
+        return $violations;
+    }
+
+    /**
+     * @psalm-return list<string>
+     */
+    public static function globPaths(array $paths) : array
+    {
+        $pathNames = [];
+        foreach (self::recursive($paths) as $i => $globDir) {
+            foreach ($globDir as $file) {
+                $pathNames[] = $file->getPathname();
+            }
+        }
+
+        return $pathNames;
+    }
+
+    /**
+     * @return array<iterable<\SplFileInfo>>
+     */
+    private static function recursive(array $paths) : array
+    {
+        $globs = [];
+
+        foreach ($paths as $i => $path) {
+            $globs[$i] = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::LEAVES_ONLY);
+        }
+
+        return $globs;
+    }
+
+    private function exists(string $include) : bool
+    {
+        foreach ($this->paths as $i => $absoluteDir) {
+            $absolutePath = $absoluteDir . DIRECTORY_SEPARATOR . $include;
+            if (in_array($absolutePath, $this->getGlobPaths(), true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function getGlobPaths() : array
+    {
+        if (!$this->globPaths) {
+            $this->globPaths = self::globPaths($this->paths);
+        }
+
+        return $this->globPaths;
+    }
+}
---
+++ tests/Rule/CaseSensitiveIncludeTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OtobankTest\TwigcsExtra\Rule;
+
+use FriendsOfTwig\Twigcs\Lexer;
+use FriendsOfTwig\Twigcs\TwigPort\Source;
+use FriendsOfTwig\Twigcs\TwigPort\TokenStream;
+use FriendsOfTwig\Twigcs\Validator\Violation;
+use Otobank\TwigcsExtra\Rule\CaseSensitiveInclude;
+use PHPUnit\Framework\TestCase;
+
+class CaseSensitiveIncludeTest extends TestCase
+{
+    /**
+     * @psalm-return array<string, array<TokenStream, int>>
+     */
+    public function provideSourcePatterns() : array
+    {
+        $lexer = new Lexer();
+
+        return [
+            'success' => [
+                $lexer->tokenize(
+                    new Source(
+                        '{%include "my/includes/TPL.html" %}',
+                        'my/path/file.html.twig',
+                        ltrim(str_replace(getcwd(), '', 'my/path/file.html.twig'), '/')
+                    )
+                ),
+                0
+            ],
+            'case insensitive' => [
+                $lexer->tokenize(
+                    new Source(
+                        '{%include "my/includes/tpl.html" %}',
+                        'my/path/file.html.twig',
+                        ltrim(str_replace(getcwd(), '', 'my/path/file.html.twig'), '/')
+                    )
+                ),
+                1
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideSourcePatterns
+     */
+    public function testCheckWithoutFunctions(TokenStream $tokenStream, int $expectedCount) : void
+    {
+        $rule = new CaseSensitiveInclude(Violation::SEVERITY_WARNING, [dirname(__DIR__, 1) . '/Resources/']);
+        $violations = $rule->check($tokenStream);
+
+        $this->assertCount($expectedCount, $violations);
+    }
+}
